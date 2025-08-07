9ecfcae8be31c13fe52fe747603dab286cf94510
Diego-Diaz797 <ddiaz11@elon.edu>
2025-08-07 12:53:25 -0400

    Making the converstions section of the code This will be finally making most of the imperal/more american systems to the metric/systems that the most of the worlds uses. This will be 1. F - C 2. Feet - Meter 3. Mile - Kilometer 4. Inch - Cm 5. Acre - Square Meter 6. Pound - Kilogram 7. Gallon - Liter 8. Fluid Oz - Milliters 9. Ounce - Grams 10. Gallons/$ - Litter/$ 11. American Dollar - Country Dollar
    adding the currancy from the other countires will be mainly getting the country of orgin, then finding there currancy(using a code or list), then doing the converstion via the american dollor to the countries curancy
    
    just added the code for the miles, km, meters, and feet. that and also the design about how I want the website will be. i havent done anything to the code related to the currancy yet

--- JavaScript/converstions.js
+++ JavaScript/converstions.js
@@ -5,20 +5,109 @@ document.addEventListener("DOMContentLoaded", () =>{
 
    //retrive the selected country from localstorge
     const selectedCountry = localStorage.getItem("selectedCountry");
-
     if (selectedCountry) {
         displayedCountryDiv.textContent = `American Dollar --> ${selectedCountry} Dollar`;
-
-        //removing the selected country
-        localStorage.removeItem("selectedCountry");
     }
     else{
         displayedCountryDiv.textContent = "No country was selected or found!";
         displayedCountryDiv.className = "text-xl text-red-400 mt-4 p-4 border border-red-700 rounded-lg w-full mac-w-sm text-center";
     }
 
+    //2-way converstion, logic section
+    const feetInput = document.getElementById("feet-input"); //feet
+    const meterInput = document.getElementById("meter-input"); //meter
+    const milesInput = document.getElementById("miles-input"); //miles
+    const kmInput = document.getElementById("kilometer-input") //km
+
+    //converstion math
+
+    // feet to meters
+    const converstionFeet = (feet) => {
+        return feet * 0.3048
+    }
+
+    //meters to feet
+    const converstionMeter = (meter) => {
+        return meter * 0.3048
+    }
+
+    //miles to kilometers
+    const converstionMiles = (miles) => {
+        return miles * 0.621371
+    }
+
+    //kilometers to miles
+    const converstionKilometers = (km) => {
+        return km * 1.609344
+    }
+
+    //event listeners
+
+    //for feet
+    feetInput.addEventListener("input", () =>{
+        const feetValue = parseFloat(feetInput.value);
+
+        if (!isNaN(feetValue)){
+            //converting meters and updating the meters field
+            const metersValue = converstionMeter(feetValue);
+            meterInput.value = metersValue.toFixed(4);
+        }
+        else{
+            //if input is not a number
+            meterInput.value = "";
+        }
+    });
+
+    //for meters
+    meterInput.addEventListener("input", () =>{
+        const meterValue = parseFloat(meterInput.value);
+
+        if (!isNaN(meterValue)){
+            //converting meters to feet
+            const feetValue = converstionFeet(meterValue);
+            feetInput.value = feetValue.toFixed(4);
+        }
+        else{
+            //if input is not a number
+            feetInput.value = "";
+        }
+    });
+
+    //for miles
+    milesInput.addEventListener("input", () =>{
+        const mileValue = parseFloat(milesInput.value);
+
+        if (!isNaN(mileValue)){
+            //converting miles to km
+            const kmValue = converstionMiles(mileValue);
+            kmInput.value = kmValue.toFixed(4);
+        }
+        else{
+            //no number was entered
+            kmInput.value = "";
+        }
+    });
+
+    //for km
+    kmInput.addEventListener("input", () =>{
+       const kmValue = parseFloat(kmInput.value);
+
+       if(!isNaN(kmValue)){
+           //converting km to miles
+           const mileValue = converstionKilometers(kmValue);
+           milesInput.value = mileValue.toFixed(4);
+       }
+       else{
+           //no number was entered
+           milesInput.value = "";
+       }
+    });
+
     //add event listener that will go back to the last screen
     goBackButton.addEventListener("click", () => {
+        //removing the country onClicked
+        localStorage.removeItem("selectedCountry");
+        //moves back to the selecting a country
        window.location.href = "Index.html";
     });
 });
