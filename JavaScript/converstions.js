31550853d768d131bc6bb7fb7837fea2bd61662d
Diego-Diaz797 <ddiaz11@elon.edu>
2025-08-08 11:50:46 -0400

    Making the converstions section of the code This will be finally making most of the imperal/more american systems to the metric/systems that the most of the worlds uses. This will be 1. F - C 2. Feet - Meter 3. Mile - Kilometer 4. Inch - Cm 5. Acre - Square Meter 6. Pound - Kilogram 7. Gallon - Liter 8. Fluid Oz - Milliters 9. Ounce - Grams 10. Gallons/$ - Litter/$ 11. American Dollar - Country Dollar
    adding the currancy from the other countires will be mainly getting the country of orgin, then finding there currancy(using a code or list), then doing the converstion via the american dollor to the countries curancy
    
    Added acre to squre metters, inches to cm, and F to C. i also added a button that clears the data that is entered.

--- JavaScript/converstions.js
+++ JavaScript/converstions.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () =>{
    //getting the button and display area elements from the html
    const displayedCountryDiv = document.getElementById("display-country");
    const goBackButton = document.getElementById("go-back-button");
+   const clearDataButton = document.getElementById("clear-data-button");
 
    //retrive the selected country from localstorge
     const selectedCountry = localStorage.getItem("selectedCountry");
@@ -18,6 +19,12 @@ document.addEventListener("DOMContentLoaded", () =>{
     const meterInput = document.getElementById("meter-input"); //meter
     const milesInput = document.getElementById("miles-input"); //miles
     const kmInput = document.getElementById("kilometer-input") //km
+    const inchInput = document.getElementById("inch-input"); //inches
+    const cmInput = document.getElementById("cm-input"); //cm
+    const acreInput = document.getElementById("Acre-input"); //acre
+    const sqMeterInput = document.getElementById("SqMeter-input"); //Square Meter
+    const fahrenheitInput = document.getElementById("fahrenheit-input"); //fahrenheit
+    const celsiusInput = document.getElementById("celsius-input"); //celsius
 
     //converstion math
 
@@ -39,6 +46,36 @@ document.addEventListener("DOMContentLoaded", () =>{
     //kilometers to miles
     const converstionKilometers = (km) => {
         return km * 1.609344
+    }
+
+    //inches to cm
+    const converstionInches = (inch) => {
+        return inch * 2.54
+    }
+
+    //cm to inches
+    const converstionCentimeters = (cm) =>{
+        return cm / 2.54
+    }
+
+    //acres to square meters
+    const converstionAcre = (acre) => {
+        return acre * 4046.86
+    }
+
+    //square meters to acres
+    const convertionSqMeters = (m2) => {
+        return m2 / 4046.86
+    }
+
+    //fahrenheit to celsius
+    const convertionFahrenheit = (fahrenheit) => {
+        return (fahrenheit - 32) / 1.8
+    }
+
+    //celsius to fahrenheit
+    const converstionCelsius = (celsius) => {
+        return celsius * 1.8 + 32
     }
 
     //event listeners
@@ -101,6 +138,102 @@ document.addEventListener("DOMContentLoaded", () =>{
            //no number was entered
            milesInput.value = "";
        }
+    });
+
+    //for inches
+    inchInput.addEventListener("input", () =>{
+       const inchValue = parseFloat(inchInput.value);
+
+       if(!isNaN(inchValue)){
+           //converting inches to cm
+           const cmValue = converstionInches(inchValue);
+           cmInput.value = cmValue.toFixed(4);
+       }
+       else{
+           //nothing was found
+           cmInput.value = "";
+       }
+    });
+
+    //for cm
+    cmInput.addEventListener("input", () =>{
+       const cmValue = parseFloat(cmInput.value);
+
+       if(!isNaN(cmValue)){
+           //converting to inches
+           const inchValue = converstionCentimeters(cmValue);
+           inchInput.value = inchValue.toFixed(4);
+       }
+       else{
+           //nothing was found
+           inchInput.value = "";
+       }
+    });
+
+    //for acre
+    acreInput.addEventListener("input", () =>{
+        const acreValue = parseFloat(acreInput.value);
+
+        if(!isNaN(acreValue)){
+            const m2Value = convertionSqMeters(acreValue);
+            sqMeterInput.value = m2Value.toFixed(4);
+        }
+        else{
+            sqMeterInput.value = "";
+        }
+    });
+
+    //for m2
+    sqMeterInput.addEventListener("input", () =>{
+        const sqMeterValue = parseFloat(sqMeterInput.value);
+
+        if(!isNaN(sqMeterValue)){
+            const acreValue = converstionAcre(sqMeterValue);
+            acreInput.value = acreValue.toFixed(4);
+        }
+        else{
+            acreInput.value = "";
+        }
+    });
+
+    //for fahrenheit
+    fahrenheitInput.addEventListener("input", () =>{
+       const fahrenheitValue = parseFloat(fahrenheitInput.value);
+
+       if(!isNaN(fahrenheitValue)){
+           const celsiusValue = convertionFahrenheit(fahrenheitValue);
+           celsiusInput.value = celsiusValue.toFixed(2);
+       }
+       else{
+           celsiusInput.value = "";
+       }
+    });
+
+    //for celsius
+    celsiusInput.addEventListener("input", () =>{
+       const celsiusValue = parseFloat(celsiusInput.value);
+
+       if(!isNaN(celsiusValue)){
+           const farhenheitValue = converstionCelsius(celsiusValue);
+           fahrenheitInput.value = farhenheitValue.toFixed(2);
+       }
+       else{
+           fahrenheitInput.value = "";
+       }
+    });
+
+    //adding an event listener that will clearn all the data
+    clearDataButton.addEventListener("click", () =>{
+       feetInput.value = "";
+       meterInput.value = "";
+       milesInput.value = "";
+       kmInput.value = "";
+       inchInput.value = "";
+       cmInput.value = "";
+       acreInput.value = "";
+       sqMeterInput.value = "";
+       fahrenheitInput.value = "";
+       celsiusInput.value = "";
     });
 
     //add event listener that will go back to the last screen
