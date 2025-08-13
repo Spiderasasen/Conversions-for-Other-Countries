946c07bbdc0081a53cfd61ca7e01955fb1e38d95
Diego-Diaz797 <ddiaz11@elon.edu>
2025-08-13 12:18:31 -0400

    Currancy conversions this is where we will make the new text file of all the currancy's that i have in my current list of countries. here we will 1. match the country with the correct currancy in the conversion section of the code 2. add either an api or a gernal rule of how much 1 usd per the currancy of the contry selected by the user. 3. The user will see the converstion of both the curency of the country selected to usd and how much 1 liter of gas will cost if its was converted to usd per gallon.    a. and vise versa 4. when the button (Go Back) is pushed, both the country and the currancy will be removed from local stroage.
    adding the clear button and also adding all the correct numbers for the us territories and also seeing if the if the country is Peruto rico, it will be the only country with a conversion from liters to gallons

--- JavaScript/currency.js
+++ JavaScript/currency.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    //clear button
+    const clearDataButton = document.getElementById("clear-data-button");
+
+    //getting the selected country
+    const selectedCountry = localStorage.getItem("selectedCountry");
+
     //getting the selected countries currency
     const selectedCurrency = localStorage.getItem("selectedCurrency");
 
@@ -28,4 +34,89 @@ document.addEventListener("DOMContentLoaded", () => {
         currencyInput.placeholder = `Enter value in ${selectedCurrency}`;
         litersCurrencyInput.placeholder = `Enter value in Liter/${selectedCurrency}`;
     }
-})
+
+    //if the country is a us territory then replace the placeholder and label for the gas to gallon exp. Puerto Rico
+    if (selectedCountry === "British Virgin Islands" || selectedCountry === "U.S Virgin Islands" || selectedCountry === "Guam" || selectedCountry === "Northern Mariana Islands" || selectedCountry === "American Samoa") {
+        litersCurrencyInput.placeholder = `Enter Value in Gallons/${selectedCountry}`;
+    }
+
+    /*checking on the currency*/
+
+    //if the currency is the usa dollar
+    if (selectedCurrency === "US Dollar"){
+        //function for gallons to liters (for pureto rico)
+
+        //gallons to liters
+        const gallonToLiters = (gallonsPrice) => {
+            return gallonsPrice / 3.78541
+        }
+
+        //liters to gallon
+        const literToGallons = (litersPrice) => {
+            return litersPrice * 3.78541
+        }
+
+        //copying the number from one side to the other
+        //usa
+        usdInput.addEventListener("input", () =>{
+            //getting the number from the usd section
+            const usdValue = parseFloat(usdInput.value);
+
+            //setting the currency input and placing it at the other end
+            currencyInput.value = usdValue.toFixed(2);
+        });
+        //usa territory
+        currencyInput.addEventListener("input", () =>{
+            //getting the number from the us territory
+            const currencyValue = parseFloat(currencyInput.value);
+
+            //setting the number into the other place
+            usdInput.value = currencyValue.toFixed(2);
+        });
+
+        //checking if the US territory is Puerto Rico
+        if (selectedCountry === "Puerto Rico"){
+            //getting the gallons price
+            gallonsUSDInput.addEventListener("input", () =>{
+                //gettting the number of the price of per gallon
+                const gallonsUSDValue = parseFloat(gallonsUSDInput.value);
+
+                //converting from gallons to liters
+                const litersUSDVale = gallonToLiters(gallonsUSDValue);
+                litersCurrencyInput.value = litersUSDVale.toFixed(2);
+            });
+
+            //getting the liters of the price to gallons
+            litersCurrencyInput.addEventListener("input", () =>{
+                //getting the price of the liters for gas
+                const litersUSDValue = parseFloat(litersCurrencyInput.value);
+
+                //converting from liters to gallons
+                const gallonsValue = literToGallons(litersUSDValue);
+                gallonsUSDInput.value = gallonsValue.toFixed(2);
+            });
+        }
+        else{
+            //just copying and pasting everything in one side to the other
+            gallonsUSDInput.addEventListener("input", () =>{
+                const gallonsValue = parseFloat(gallonsUSDInput.value);
+                litersCurrencyInput.value = gallonsValue.toFixed(2);
+            });
+            litersCurrencyInput.addEventListener("input", () =>{
+                const litersValue = parseFloat(litersCurrencyInput.value);
+                gallonsUSDInput.value = litersValue.toFixed(2);
+            });
+        }
+    }
+
+    //if the currency is the euro
+
+
+    //adding an event listener that will clearn all the data
+    clearDataButton.addEventListener("click", () =>{
+        gallonsUSDInput.value = "";
+        litersCurrencyInput.value = "";
+        usdInput.value = "";
+        currencyInput.value = "";
+    });
+});
